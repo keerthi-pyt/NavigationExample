ee2f8050757407e2af8a2f32f26f6c17aa6d39f4
Keerthi <keerthi@pickyourtrail.com>
2023-01-02 14:52:14 +0530

    fix: changed title

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -15,7 +15,7 @@ export const Login:FunctionComponent<LoginProps> =({navigation})=>{
     const { login } = useContext(AuthContext)
     return (
         <Center>
-            <Text>Login screen</Text>
+            <Text>Login</Text>
             <Button title='Log in' onPress={()=>login()}/>
             <Button title='Go to register' onPress={()=>navigation.navigate('Register')}/>
         </Center>
